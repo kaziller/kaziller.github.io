3686297557563ee2bdfad00862a1cdcb8ddfd87b
Thanh <tenhcong@campus.tu-berlin.de>
2017-07-08 21:12:41 +0200

    TEst

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 var SimplePeer = require('simple-peer')
 
 // get video/voice stream
-navigator.getUserMedia({ video: true, audio: true }, gotMedia, function () {})
+// navigator.getUserMedia({ video: true, audio: true }, gotMedia, function () {})
 
-function gotMedia (stream) {
-  var peer1 = new SimplePeer({ initiator: true, stream: stream })
+function gotMedia (data) {
+  // var peer1 = new SimplePeer({ initiator: true, stream: stream })
   var peer2 = new SimplePeer()
 
-  peer1.on('signal', function (data) {
-    peer2.signal(data)
-  })
+  // peer1.on('signal', function (data) {
+  //   peer2.signal(data)
+  // })
 
-  peer2.on('signal', function (data) {
-    peer1.signal(data)
-  })
+  // peer2.on('signal', function (data) {
+  //   peer1.signal(data)
+  // })
+
+  peer2.signal(data);
 
   peer2.on('stream', function (stream) {
+    console.log(stream);
     // got remote video stream, now let's show it in a video tag
     var video = document.querySelector('video')
     video.src = window.URL.createObjectURL(stream)
     video.play()
+    console.log('video created');
   })
 }
+
+var signalhub = require('signalhub')
+var hub = signalhub('my-app-name', [
+  'https://cloudbrowsersignalingserver.herokuapp.com/'
+])
+
+hub.subscribe('my-channel')
+  .on('data', function (message) {
+    console.log('new message received', message)
+    if(message.type === 'offer') {
+      gotMedia(message);
+    }
+  });
