38d0e733a074ab56779cc2793530041e6ba9423d
Thanh <tenhcong@campus.tu-berlin.de>
2017-07-10 11:24:48 +0200

    sfsdg

--- test.js
+++ test.js
@@ -20,14 +20,8 @@ var p = new Peer({ initiator: false, trickle: false })
 p.on('error', function (err) { console.log('error', err) })
 
 p.on('signal', function (data) {
-  console.log('SIGNAL', JSON.stringify(data))
-  document.querySelector('#outgoing').textContent = JSON.stringify(data)
+  console.log('SIGNAL', JSON.stringify(data));
   hub.broadcast('my-channel', JSON.stringify(data));
-})
-
-document.querySelector('form').addEventListener('submit', function (ev) {
-  ev.preventDefault()
-  p.signal(JSON.parse(document.querySelector('#incoming').value))
 })
 
 p.on('connect', function () {
